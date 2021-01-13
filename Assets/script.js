d8f00ff2fb01744f823d309417d7d919da58029f
Erum Dhukka <erumdhukka531@gmail.com>
2021-01-13 05:32:07 -0600

    yay, current weather working

--- Assets/script.js
+++ Assets/script.js
@@ -1,143 +1,151 @@
-
-
-$(document).ready(function () {
-    $("#search").on("click", function () {
-    var searchValue = $("#searchValue").val();
-    $("#searchValue").val("");
-    currentConditions(searchValue);
-    });
-
-    function currentConditions(searchValue);
-    {
-        $("#currentWeather-view").empty();
-        var APIKey = "76867f1d9d820e6fd45b355d5a55ddc8";
-        console.log(APIKey);
-        
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey;
-        console.log(queryURL, "this is with search results");
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-            var cityName = $("<h2>").html("City: " + response.name);
-            console.log(city);
-
-            var tempF = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2) + "°F";
-            tempF = tempF.toString().replace(/^(\d+)?([.]?\d{0,2})?$/, "");
-            console.log(tempF);
-    
-            var iconImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
-            iconImage.attr("width", 50);
-            console.log(iconImage);
-            var humidityCity = $("<h4>").html("Humidity: " + response.main.humidity);
-            console.log(humidityCity)
-            var windSpeedCity = $("<h4>").html("Wind Speed: " + response.wind.speed);
-            console.log(windSpeedCity);
-            $("#currentWeather-view").append(cityName, iconImage, tempF, humidityCity, windSpeedCity);
-        }); 
-    };
-    
-            
-});
-
-
-
-
-$(document).ready(function () {
+   // telling html to run after input has been given
+   $(document).ready(function () {
     $("#search").on("click", function () {
       // get element value with jquery
       var searchValue = $("#searchValue").val();
       $("#searchValue").val("");
       // run logic to get data from API
       // tutor help
-      searchWeather(searchValue);
+      console.log (searchValue, "button works");
+      weatherSearch(searchValue);
     });
 
-//     var cityNames = [""];
-
-// //   function createCityButton() {
-// //     cityNames.forEach(function (city) {
-     
-// //       var cityNameBtn = $("<button>").attr("data-type", city);
-// //       cityNameBtn.text(city);
-// //       cityNameBtn.attr("type", "button");
-// //       cityNameBtn.addClass("city");
-// //     });
-// //   }
-
-//   function addButton(name) {
-//     var btn = $("<button>").text(name);
-//     $(".w3-container").append(btn)
-//   }
+    $("searchValue").keypress(function (event) {
+    // try to use keyCode === 13
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      $("#search").click();
+    }
+    });
 
  
-//   console.log($('[name="add-city-button"]'));
+    // ************************Need the below line 76, 78, 85 to recall search
+    $('[name="add-city-button"]').on("click", function () {
+    // need prevent default for page to load
+    event.preventDefault();
+  });
 
-//   $('[name="add-city-button"]').on("click", function () {
-//     // need prevent default for page to load
-//     event.preventDefault();
+   
 
-//     console.log("TEST EVENT LISTENER");
-//     $('[name="add-city"]').val();
-//     // need to add the bottom to get value in a button****************I think this is missing piece.
-//     cityNames.push($('[name="add-city"]').val());
-//     $(".city-container").empty();
-//     createCityButton();
-//   });
-//   // trying to get buttons to click and show city temp
-// //   $("#cityId").on("click", function () {
-// //     var city = $(this).attr("data-type");
-// //     console.log(city);
-// //     console.log($("#searchValue").val());
-// //     weatherSearch(searchValue)
-// //   });
+
+function weatherSearch(searchValue) {
+  var APIKey = "76867f1d9d820e6fd45b355d5a55ddc8";
+  console.log(APIKey);
+
+  var queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey;
+  console.log(queryURL, "this is with search results");
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".humidity").text(`Humidity: ${response.main.humidity}%`);
+
+      
+      // Convert the temp to fahrenheit
+      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+      // add temp content to html
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + tempF);
+
+  
+    });
+  }
+  });
+
+
+
+
+
+// $(document).ready(function () {
+//     // $("#search").on("click", function () {
+//     // var searchValue = $("#searchValue").val();
+//     // $("#searchValue").val("");
+//     // currentConditions(searchValue);
+//     // });
+
+// var cityNames = ["Houston"];
+//   function createCityButton() {
+//     cityNames.forEach(function (city) {
+//       var cityNameBtn = $("<button>").attr("data-type", city);
+//       cityNameBtn.text(city);
+//       cityNameBtn.attr("type", "button");
+//       cityNameBtn.addClass("city");
+//     });
+//   }
+//   console.log("TEST EVENT LISTENER");
+//   $('[name="add-city"]').val();
+// //   cityNames.push($('[name="add-city"]').val());
+//   $(".city-container").empty();
+//   createCityButton();
+
 
 //   function weatherSearch(searchValue) {
-//             var APIKey = "76867f1d9d820e6fd45b355d5a55ddc8";
-//             console.log(APIKey);
-
-//             var queryURL =
-//             "https://api.openweathermap.org/data/2.5/weather?q=" +
-//             searchValue +
-//             "&appid=" +
-//             APIKey;
-
-//         console.log(queryURL, "this is with search results");
-
-//         $.ajax({
-//             url: queryURL,
-//             method: "GET",
-//         }).then(function (response) {
-//             // if this city is already in the array
-//             // if this city is not in the array
-//             // only then add this to history
-//             history.push(searchValue);
-//             localStorage.setItem("history", JSON.stringify(history));
-//             addButton(searchValue);
-
-//             console.log("This is the weather data: ", response);
-
-//             $(".city").html(`<h2>${response.name} ( ${moment().format("MMMM DD, YYYY")} ) </h2>`);
-//             $("#icon0").attr("src","https://openweathermap.org/img/wn/" + response.weather[0].icon +"@2x.png");
-//             localStorage.setItem("city", `${response.name}`);
-//             $(".wind").text(`Wind Speed: ${response.wind.speed} MPH`);
-//             $(".humidity").text(`Humidity: ${response.main.humidity}%`);
-//             let temp = `Temperature (k): ${response.main.temp}`;
-//                 temp += `<br />Temperature (F): ${Math.round((response.main.temp - 273.15) * 1.8 + 32)}°F`;
-//                  $(".temp").html(temp);
-//                  lat = response.coord.lat;
-//                  lon = response.coord.lon;
-//                 console.log("This is lat and lon from weather function", lat, lon);
+// // This is our API key. Add your own API key between the ""
+// var APIKey = "76867f1d9d820e6fd45b355d5a55ddc8";
+// console.log(APIKey);
+// var searchValue = $("#searchValue").val();
+// $("#searchValue").val("");
 
 
-//         });
-
-//     }
 
 
+// // Here we are building the URL we need to query the database
+// // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+
+        
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey;
+// console.log(queryURL, "this is with search results");
+
+// // We then created an AJAX call
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function(response) {
+//     history.push(searchValue);
+//     localStorage.setItem("history", JSON.stringify(history));
+//     addButton(searchValue);
+//     console.log("This is the weather data: ", response);
+
+//   // Create CODE HERE to Log the queryURL
+//   // Create CODE HERE to log the resulting object
+//   console.log(response);
+//   // Create CODE HERE to calculate the temperature (converted from Kelvin)
+//   // Create CODE HERE to transfer content to HTML
+//   $('.city').html(`<h1>${response.name} Weather Details</h1>`);
+//   $('.wind').text(`Wind Speed: ${response.wind}`);
+//   $('.humidity').text(`Humidity: ${response.main.humidity}`);
+//   let temp = `Temperature (k): ${response.main.temp}`;
+//   temp += `<br />Tempature (F): ${(response.main.temp - 273.15) * 1.80 + 32}`;
+//   $('.temp').html(temp);
+//   // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+//   // Create CODE HERE to dump the temperature content into HTML
 
 // });
+// }
+
+// });
+
+
+
 
